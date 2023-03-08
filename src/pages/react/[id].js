c63e27c383b59aecad8cbc9a4d8f1e84028ebf12
이재연 <wodus5677@gmail.com>
2023-03-08 15:16:01 +0900

    solve react page error

--- src/pages/react/[id].js
+++ src/pages/react/[id].js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { getPostData, getCategoryPostIds } from "../../lib/posts";
 
 import { initializeApp } from "firebase/app";
@@ -7,6 +8,9 @@ import {
   setDoc,
   updateDoc,
   arrayUnion,
+  getDoc,
+  deleteField,
+  arrayRemove,
 } from "firebase/firestore";
 
 import Layout from "../../components/Layout";
@@ -26,6 +30,15 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+const today = new Date();
+let year = today.getFullYear(); // 년도
+let month =
+  today.getMonth() + 1 < 10
+    ? "0" + parseInt(today.getMonth() + 1)
+    : today.getMonth() + 1;
+let day = today.getDate() < 10 ? "0" + today.getDate() : today.getDate();
+const date = year + "/" + month + "/" + day;
+
 export async function getStaticPaths() {
   const paths = getCategoryPostIds("react");
   return {
@@ -44,20 +57,95 @@ export async function getStaticProps({ params }) {
 }
 
 const Post = ({ postData }) => {
+  const [commentArray, setCommentArray] = useState([]);
+  const [visibleReplyWrite, setVisibleReplyWrite] = useState(false);
+  const postRef = doc(db, "posts/", postData.title);
+
+  useEffect(() => {
+    let postDocSnap;
+    const getPostDoc = async () => {
+      postDocSnap = await getDoc(postRef);
+      if (postDocSnap.exists()) {
+        console.log("postDocSnap.data().comments", postDocSnap.data().comments);
+        if (postDocSnap.data().comments) {
+          setCommentArray(postDocSnap.data().comments);
+        } else {
+          setCommentArray([]);
+        }
+      } else {
+        console.log("no document");
+      }
+    };
+
+    getPostDoc();
+  }, []);
+
   const writeComment = (name, password, comment) => {
-    const postRef = doc(db, "posts/", postData.title);
+    const commentItem = {
+      id: name + today.getMilliseconds(),
+      name,
+      password,
+      comment,
+      postId: postData.id,
+      date,
+      replys: [],
+    };
+    if (commentArray.length === 0) {
+      setDoc(postRef, {
+        comments: [commentItem],
+      });
+      setCommentArray([commentItem]);
+    } else if (commentArray.length > 0) {
+      updateDoc(postRef, {
+        comments: arrayUnion(commentItem),
+      });
+      setCommentArray([...commentArray, commentItem]);
+    }
+  };
+
+  const writeReply = (name, password, comment, commentId, selectedIndex) => {
+    const replyItem = {
+      id: name + today.getMilliseconds(),
+      name,
+      password,
+      comment,
+      postId: postData.id,
+      date,
+      commentId: commentId,
+    };
+
+    const tmpCommentArray = [...commentArray];
+    tmpCommentArray[selectedIndex].replys.push(replyItem);
+
+    setCommentArray(tmpCommentArray);
     updateDoc(postRef, {
-      comments: arrayUnion({
-        comment: comment,
-        name: name,
-        password: password,
-      }),
+      comments: tmpCommentArray,
+    });
+  };
+
+  const removeComment = (selectedIndex) => {
+    console.log("removeComment selectedIndex", selectedIndex);
+    const selectedComment = commentArray[selectedIndex];
+    const removedCommentArray = commentArray.filter((item, index) => {
+      return selectedIndex !== index;
+    });
+
+    setCommentArray(removedCommentArray);
+
+    updateDoc(postRef, {
+      comments: arrayRemove(selectedComment),
     });
   };
 
   return (
     <Layout>
-      <PostDetail postData={postData} />
+      <PostDetail
+        postData={postData}
+        writeComment={writeComment}
+        commentArray={commentArray}
+        removeComment={removeComment}
+        writeReply={writeReply}
+      />
     </Layout>
   );
 };
