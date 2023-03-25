9ab925ae531ea7b628b09be1f5a5f1c5f7ae583f
이재연 <wodus5677@gmail.com>
2023-03-25 18:25:33 +0900

    remove styled component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,34 +1,24 @@
 import { useCallback } from "react";
-import styled from "styled-components";
 import style from "./styles/post-list.module.css";
 import PostItem from "./PostItem";
 
 import { motion, useMotionValue, useTransform } from "framer-motion";
-
-const MotionDiv = styled(motion.div)`
-  @media (min-width: 470px) {
-    margin-left: ${(props) => (props.index % 3 !== 0 ? "1rem" : "0")};
-  }
-
-  @media (min-width: 470px) and (max-width: 900px) {
-    margin-left: ${(props) => (props.index % 2 !== 0 ? "1rem" : "0")};
-  }
-`;
 
 const PostList = ({ postsData, isMobile }) => {
   return (
     <div className={style.container}>
       <div className={style.postListWrapper}>
         {postsData.map((post, index) => (
-          <MotionDiv
-            index={index}
-            initial={false}
-            whileHover={isMobile ? "" : { scale: 1.074 }}
-            transition={isMobile ? "" : { type: "linear", duration: 0.22 }}
-            key={index}
-          >
-            <PostItem post={post} key={index} />
-          </MotionDiv>
+          <div className={style.postItemWrapper} key={index}>
+            <motion.div
+              index={index}
+              initial={false}
+              whileHover={isMobile ? "" : { scale: 1.074 }}
+              transition={isMobile ? "" : { type: "linear", duration: 0.22 }}
+            >
+              <PostItem post={post} key={index} />
+            </motion.div>
+          </div>
         ))}
         {postsData.length % 3 === 2 ? (
           <div className={style.emptyPostItemWrapper} />
