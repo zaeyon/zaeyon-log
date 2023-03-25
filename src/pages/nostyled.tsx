0c862aa29e7737a3e67b98e01241043b0d221a56
이재연 <wodus5677@gmail.com>
2023-03-25 14:13:37 +0900

    test

---
+++ src/pages/nostyled.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/test.module.css'
+
+interface props {
+    
+}
+
+const Nostyled:React.FC<props> = ({}) => {
+
+    return (
+        <div>
+            <div className={styles.Navbar}>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Nostyled
